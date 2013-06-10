de2b9711ffe2715e24180f57a1f7ba1aceaa3e77
Andy Perkins <andy.perkins@thebluehive.com>
2013-06-10 12:39:28 +0100

    all working now

--- web/view/cardViewItem.dart
+++ web/view/cardViewItem.dart
@@ -12,19 +12,19 @@ class CardViewItem
     [<input type="checkbox" [...] />] <!-- if in label -->
   </[a|label|abbr|div|span]>
   */
-  
+
   LIElement cardElement ;
-  
+
   CardViewItem( CardVO c )
   {
-     this.cardElement = new LIElement( ) ;
-    
-     DivElement de = new DivElement ();
-     
+     this.cardElement = new LIElement() ;
+
+     DivElement de = new DivElement();
+
      String divClass = "card rank-${c.rank} ${c.suit}" ;
-     
+
      de.classes.add( divClass );
-     
+
      SpanElement s1 = new SpanElement();
      s1.classes.add( "rank");
      s1.text = c.rank.toUpperCase() ;
@@ -32,10 +32,10 @@ class CardViewItem
      SpanElement s2 = new SpanElement();
      s2.classes.add( "suit" );
      s2.innerHtml = "&${c.suit};" ;
-     
+
      de.children.add( s1 ) ;
      de.children.add( s2 ) ;
-     
+
      this.cardElement.children.add( de ) ;
   }
 }
